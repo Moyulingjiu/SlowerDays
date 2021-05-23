5aa37c9fc05f4a345f8c3e1bfd6003ecdc77d80f
FangFang270 <1249808893@qq.com>
2021-05-23 23:21:18 +0800

    增加了一些商品页面

--- src/miniprogram/pages/paper/paper.js
+++ src/miniprogram/pages/paper/paper.js
@@ -11,7 +11,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    imageURL:"cloud://cloud1-9g6mp0559beaec2a.636c-cloud1-9g6mp0559beaec2a-1305792439/test/images/cloudstore/sale2.jpg",
+    imageURL:"cloud://cloud1-9g6mp0559beaec2a.636c-cloud1-9g6mp0559beaec2a-1305792439/test/images/cloudstore/sale3.jpg",
   },
 
   /**
--- src/miniprogram/pages/stamps/stamps.js
+++ src/miniprogram/pages/stamps/stamps.js
@@ -11,7 +11,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    imageURL:"cloud://cloud1-9g6mp0559beaec2a.636c-cloud1-9g6mp0559beaec2a-1305792439/test/images/cloudstore/stamp3.jpg",
+    imageURL:"cloud://cloud1-9g6mp0559beaec2a.636c-cloud1-9g6mp0559beaec2a-1305792439/test/images/cloudstore/stamp2.jpg",
   },
 
   /**
